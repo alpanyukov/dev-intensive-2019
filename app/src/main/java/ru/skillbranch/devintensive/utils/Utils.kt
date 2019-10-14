743159eedcb658c92e1f24c2cc5fb6cc6591c084
Alexander <kurono@bk.ru>
2019-10-14 13:03:35 +0300

    add parseFullName tests

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -4,8 +4,11 @@ object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?>{
         val parts= fullName?.trim()?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
+        var firstName = parts?.getOrNull(0)
+        firstName = if (firstName == "") null else firstName
+
+        var lastName = parts?.getOrNull(1)
+        lastName = if (lastName == "") null else lastName
 
         return firstName to lastName
     }
