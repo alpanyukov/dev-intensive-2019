b55b85b876c4c296d278f5354d75a8c287aca233
Alexander <kurono@bk.ru>
2019-10-15 17:14:55 +0300

    fix humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
 
 const val SECOND = 1000
@@ -15,30 +16,52 @@ fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
 }
 
 /**
-    Преобразует по принципу
-    0с - 1с "только что"
-    1с - 45с "несколько секунд назад"
-    45с - 75с "минуту назад"
-    75с - 45мин "N минут назад"
-    45мин - 75мин "час назад"
-    75мин 22ч "N часов назад"
-    22ч - 26ч "день назад"
-    26ч - 360д "N дней назад"
-    >360д "более года назад"
+Преобразует по принципу
+0с - 1с "только что"
+1с - 45с "несколько секунд назад"
+45с - 75с "минуту назад"
+75с - 45мин "N минут назад"
+45мин - 75мин "час назад"
+75мин 22ч "N часов назад"
+22ч - 26ч "день назад"
+26ч - 360д "N дней назад"
+>360д "более года назад"
  */
-fun Date.humanizeDiff(date: Date = Date()): String{
-    val timeDiff = this.time - date.time
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val timeDiff = date.time - this.time
+    val isPast = timeDiff > 0
 
-    return when(timeDiff){
-        in 0 until SECOND -> "только что"                                                                   // 0с - 1с "только что"
-        in SECOND until 45 * SECOND -> "несколько секунд назад"                                             // 1с - 45с "несколько секунд назад" "через несколько секунд"
-        in 45 * SECOND until 75 * SECOND -> "минуту назад"                                                  // 45с - 75с "минуту назад" "через минуту"
-        in 75 * SECOND until 45 * MINUTE -> "${(timeDiff / MINUTE.toFloat()).roundToInt()} минут назад"     // 75с - 45мин "N минут назад" "через "
-        in 45 * MINUTE until 75 * MINUTE -> "час назад"                                                     // 45мин - 75мин "час назад"
-        in 75 * MINUTE until 22 * HOUR -> "${(timeDiff / HOUR.toFloat()).roundToInt()} часов назад"         // 75мин 22ч "N часов назад"
-        in 22 * HOUR until 26 * HOUR -> "день назад"                                                        // 22ч - 26ч "день назад"
-        in 26 * HOUR until 360 * DAY.toLong() -> "${(timeDiff / DAY.toFloat()).roundToInt()} дней назад"    // 26ч - 360д "N дней назад"
-        else -> "более года назад"
+    return when (val absoluteDiff = timeDiff.absoluteValue) {
+        in 0 until SECOND -> "только что"
+
+        in SECOND until 45 * SECOND ->
+            if (isPast) "несколько секунд назад" else "через несколько секунд"
+
+        in 45 * SECOND until 75 * SECOND -> if (isPast) "минуту назад" else "через минуту"
+
+        in 75 * SECOND until 45 * MINUTE -> {
+            val minutes = (absoluteDiff / MINUTE.toFloat()).roundToInt()
+            val plural = TimeUnits.MINUTE.plural(minutes)
+            return if (isPast) "$minutes $plural назад" else "через $minutes $plural"
+        }
+
+        in 45 * MINUTE until 75 * MINUTE -> if(isPast) "час назад" else "через час"
+
+        in 75 * MINUTE until 22 * HOUR -> {
+            val hours = (absoluteDiff / HOUR.toFloat()).roundToInt()
+            val plural = TimeUnits.HOUR.plural(hours)
+            return if (isPast) "$hours $plural назад" else "через $hours $plural"
+        }
+
+        in 22 * HOUR until 26 * HOUR -> if(isPast) "день назад" else "через день"
+
+        in 26 * HOUR until 360 * DAY.toLong() -> {
+            val days = (absoluteDiff / DAY.toFloat()).roundToInt()
+            val plural =TimeUnits.DAY.plural(days)
+            return if(isPast) "$days $plural назад" else "через $days $plural"
+        }
+
+        else -> if(isPast) "более года назад" else "более чем через год"
     }
 }
 
@@ -46,7 +69,7 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     var time = this.time
     val longValue = value.toLong()
 
-    time += when(units){
+    time += when (units) {
         TimeUnits.SECOND -> longValue * SECOND
         TimeUnits.MINUTE -> longValue * MINUTE
         TimeUnits.HOUR -> longValue * HOUR
@@ -60,8 +83,34 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+    SECOND {
+        override fun plural(value: Int): String = when (value % 10) {
+            1 -> "секунда"
+            in 2..4 -> "секунды"
+            else -> "cекунд"
+        }
+    },
+    MINUTE {
+        override fun plural(value: Int): String = when (value % 10) {
+            1 -> "минута"
+            in 2..4 -> "минуты"
+            else -> "минут"
+        }
+    },
+    HOUR {
+        override fun plural(value: Int): String = when (value % 10) {
+            1 -> "час"
+            in 2..4 -> "часа"
+            else -> "часов"
+        }
+    },
+    DAY {
+        override fun plural(value: Int): String = when (value % 10) {
+            1 -> "день"
+            in 2..4 -> "дня"
+            else -> "дней"
+        }
+    };
+
+    abstract fun plural(value: Int): String
 }
