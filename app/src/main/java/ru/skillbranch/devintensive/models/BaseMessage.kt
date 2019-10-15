7052e47bf6d27deea8bc19208d98dbbcf6964044
Alexander Panyukov <olenevodec@gmail.com>
2019-10-15 20:37:44 +0300

    Исправил BaseMessage

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -3,37 +3,45 @@ package ru.skillbranch.devintensive.models
 import java.util.*
 
 abstract class BaseMessage(
-    val id: String,
-    val from: User?,
-    val chat: Chat,
-    val isIncoming: Boolean = false,
-    val date: Date = Date()
+        val id: String,
+        val from: User?,
+        val chat: Chat,
+        val isIncoming: Boolean = false,
+        val date: Date = Date()
 ) {
     abstract fun formatMessage(): String
 
     companion object AbstractFactory {
         var lastId = -1
-        fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String, payload: String?, isIncoming: Boolean = false): BaseMessage? {
+        fun makeMessage(
+                from: User?,
+                chat: Chat,
+                date: Date = Date(),
+                type: String,
+                payload: String?,
+                isIncoming: Boolean = false
+        ): BaseMessage {
+
             lastId++
 
-            return when(type){
+            return when (type) {
                 "text" -> TextMessage(
-                    "$lastId",
-                    from,
-                    chat,
-                    isIncoming,
-                    date,
-                    payload
+                        "$lastId",
+                        from,
+                        chat,
+                        isIncoming,
+                        date,
+                        payload
                 )
                 "image" -> ImageMessage(
-                    "$lastId",
-                    from,
-                    chat,
-                    isIncoming,
-                    date,
-                    payload
+                        "$lastId",
+                        from,
+                        chat,
+                        isIncoming,
+                        date,
+                        payload
                 )
-                else -> null
+                else -> throw java.lang.IllegalStateException()
             }
         }
     }
