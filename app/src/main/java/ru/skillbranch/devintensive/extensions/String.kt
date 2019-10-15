fb693419dbda2ee9a409b39ac7761db68803ddcc
Alexander <kurono@bk.ru>
2019-10-15 16:12:03 +0300

    add stripHTML

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -3,9 +3,16 @@ package ru.skillbranch.devintensive.extensions
 fun String.truncate(maxLength: Int = 16): String {
     val result = this.trim()
 
-    if(maxLength >= result.length) {
+    if (maxLength >= result.length) {
         return result
     }
 
-    return result.filterIndexed { index, _ -> index < maxLength}.trim() + "..."
+    return result.filterIndexed { index, _ -> index < maxLength }.trim() + "..."
+}
+
+fun String.stripHtml(): String {
+    return this.trim()
+        .replace(Regex("(<[^>]+>)"), "")
+        .replace(Regex("&.+;"), "")
+        .replace(Regex(" +"), " ")
 }
